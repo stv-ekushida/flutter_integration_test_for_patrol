e82711741c6612cd1eb7eb9ea5a73123bd2f2c63
e.kushida <e.kushida@stv-tech.co.jp>
2024-08-07 12:58:12 +0900

    feat: ios pilot demo

---
+++ integration_test/example_test.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pilot_test/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
+
+void main() {
+  patrolTest('demo', (PatrolIntegrationTester $) async {
+    await $.pumpWidgetAndSettle(const MyApp());
+
+    expect(
+      find.text('Flutter Demo Home Page'),
+      findsOneWidget,
+    );
+
+    expect(
+      find.text('0'),
+      findsOneWidget,
+    );
+
+    final button = find.byType(FloatingActionButton);
+
+    expect(
+      button,
+      findsOneWidget,
+    );
+
+    await $.tester.tap(button);
+
+    await $.tester.pumpAndSettle();
+
+    expect(
+      find.text('1'),
+      findsOneWidget,
+    );
+
+    await $.tester.tap(button);
+
+    await $.tester.pumpAndSettle();
+
+    expect(
+      find.text('2'),
+      findsOneWidget,
+    );
+  });
+}
